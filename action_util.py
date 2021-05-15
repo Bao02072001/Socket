682f4b52faa7db8731dbceb172eb9005ecd30072
QuocBao <81639626+Bao02072001@users.noreply.github.com>
2021-05-16 00:37:31 +0700

    Delete action_util.py

--- action_util.py
+++
@@ -1,9 +0,0 @@
-class Action:
-    send_message = "send_msg"
-    login = "login"
-    change_pwd = "change_pwd"
-    sign_up = 'register'
-    status_notify = "status_notify"
-    update_info = "update_info"
-    view_info = "view_info"
-    file_notify = "file_notify"
