4973ad01e1b2358f0fca7d80e1affb730f439e92
QuocBao <81639626+Bao02072001@users.noreply.github.com>
2021-05-15 22:51:41 +0700

    Xem thử

---
+++ action_util.py
@@ -0,0 +1,9 @@
+class Action:
+    send_message = "send_msg"
+    login = "login"
+    change_pwd = "change_pwd"
+    sign_up = 'register'
+    status_notify = "status_notify"
+    update_info = "update_info"
+    view_info = "view_info"
+    file_notify = "file_notify"
